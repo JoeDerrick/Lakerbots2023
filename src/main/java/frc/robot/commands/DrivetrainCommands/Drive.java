9ba6b32669ae119368b66eaf1f37242eb4a54d45
comicsansgood <masonman0810@gmail.com>
2023-02-10 16:24:59 -0500

    changed drive command name

---
+++ src/main/java/frc/robot/commands/DrivetrainCommands/Drive.java
@@ -0,0 +1,51 @@
+package frc.robot.commands.DrivetrainCommands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.drivetrain;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
+public class Drive extends CommandBase {    
+    private drivetrain m_Drivetrain;    
+    private DoubleSupplier translationSup;
+    private DoubleSupplier strafeSup;
+    private DoubleSupplier rotationSup;
+    private BooleanSupplier robotCentricSup;
+
+
+
+
+    
+
+    public Drive(drivetrain m_Drivetrain, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
+        this.m_Drivetrain = m_Drivetrain;
+        addRequirements(m_Drivetrain);
+
+        this.translationSup = translationSup;
+        this.strafeSup = strafeSup;
+        this.rotationSup = rotationSup;
+        this.robotCentricSup = robotCentricSup;
+    }
+
+    @Override
+    public void execute() {
+        /* Get Values, Deadband*/
+        double translationVal = MathUtil.applyDeadband(translationSup.getAsDouble(), Constants.stickDeadband);
+        double strafeVal = MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.stickDeadband);
+        double rotationVal = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.stickDeadband);
+
+        /* Drive */
+        m_Drivetrain.drive(
+            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed), 
+            rotationVal * Constants.Swerve.maxAngularVelocity, 
+            !robotCentricSup.getAsBoolean(), 
+            true
+        );
+    }
+}
