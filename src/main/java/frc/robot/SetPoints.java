7f75173e9d3ddfd70c94d0bae378e0df7cbe5df5
comicsansgood <masonman0810@gmail.com>
2023-02-20 11:56:28 -0500

    added elevator setpoints

--- src/main/java/frc/robot/SetPoints.java
+++ src/main/java/frc/robot/SetPoints.java
@@ -3,18 +3,19 @@ package frc.robot;
 public final class SetPoints {
     
     //arm setPoints
+    //NEGATIVE FOR ALL FRONT POSITIONS!!!
     public static double armHome = 0;
-    public static double armFront = 25;
+    public static double armFront = -25;
     public static double armBack = -25;
-    public static double armPlaceHybridFront = 100;
+    public static double armPlaceHybridFront = -45;
     public static double armPlaceHybridBack = -100;
-    public static double armPlaceConeMiddleFront = 250;
+    public static double armPlaceConeMiddleFront = -32;
     public static double armPlaceConeMiddleBack = -250;
     public static double armPlaceCubeMiddleFront = 200;
     public static double armPlaceCubeMiddleBack = -200;
-    public static double armPlaceConeHighFront = 350;
+    public static double armPlaceConeHighFront = -25;
     public static double armPlaceConeHighBack = -350;
-    public static double armPlaceCubeHighFront = 300;
+    public static double armPlaceCubeHighFront = 25;
     public static double armPlaceCubeHighBack = -300;
 
     //intake setPoints
@@ -22,9 +23,11 @@ public final class SetPoints {
     public static double intakeSqueezePos = 100;
 
     //Elevator setPoints
+
+    //ALL ELEVATOR SETPOINTS NEED TO BE NEGATIVE!!!!!!!!!!!!!!!!
     public static double ElevatorHome = 0;
-    public static double ElevatorMiddle = 100;
-    public static double ElevatorTop = 200;
+    public static double ElevatorMiddle = -600;
+    public static double ElevatorTop = -20;
 
     //wrist setpoints
     public static double WristHome = 0;
