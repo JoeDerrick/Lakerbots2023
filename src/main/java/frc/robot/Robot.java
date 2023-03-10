d8e829f59064246604993ad3e60f94330ad708a2
comicsansgood <masonman0810@gmail.com>
2023-03-10 12:06:36 -0500

    added reverse gryo to the tele init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.LEDCommands.Green;
 
 import frc.robot.subsystems.*;
 
@@ -35,6 +36,7 @@ public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
     private RobotContainer m_robotContainer;
+
     
 
     /**
@@ -108,6 +110,7 @@ public class Robot extends TimedRobot {
         if (m_autonomousCommand != null) {
             m_autonomousCommand.cancel();
         }
+        m_robotContainer.m_drivetrain.reverseGryo();
     }
 
     /**
